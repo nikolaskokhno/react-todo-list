d5075139f57484dad306366adbd9cd80863893b6
kokhno <kokhno.mm@gmail.com>
2020-01-09 00:11:53 +0200

    create header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <div className="header">
+            <h3>Simple todo lists</h3>
+            <h5>from ruby garage</h5>
+        </div>
+    );
+}
+
+export default Header;
