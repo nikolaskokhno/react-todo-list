0966f4fccb07fa863b16c707a383243c2f851647
kokhno <kokhno.mm@gmail.com>
2020-01-09 00:32:04 +0200

    create copyright componet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import ButtonAddProject from './components/AddProject/ButtonAddProject';
+import Copyright from './components/Copyright/Copyright';
 
 function App() {
   return (
-    <div className="app">
+    <div className="app #f57c00 orange darken-2">
       <div className="container">
         <Header />
         <ButtonAddProject />
+        <Copyright />
       </div>
     </div>
   );
---
+++ src/components/Copyright/Copyright.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Copyright = () => {
+
+    return (
+        <div className="copyright col s12">
+            &copy; Ruby Garage	
+        </div>
+    );
+}
+
+export default Copyright;
