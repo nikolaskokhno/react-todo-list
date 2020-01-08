614988c0cf61e1b674f1e308a67fd6522ece516c
kokhno <kokhno.mm@gmail.com>
2020-01-09 00:19:39 +0200

    create add button project componet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import ButtonAddProject from './components/AddProject/ButtonAddProject';
 
 function App() {
   return (
     <div className="app">
       <div className="container">
         <Header />
+        <ButtonAddProject />
       </div>
     </div>
   );
---
+++ src/components/AddProject/ButtonAddProject.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ButtonAddProject = () => {
+
+    return (
+        <div className="addProject col s12">
+            <div className="waves-effect waves-light btn #1976d2 blue darken-2">
+                <i className="material-icons left">add</i>
+                Add todo project
+            </div>
+        </div>
+    );
+}
+
+export default ButtonAddProject;
